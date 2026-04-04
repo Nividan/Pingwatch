a0d54f648f3d30a6cf59082639ccdb2cbe30819e
Niv <nividan@gmail.com>
2026-04-04 20:27:28 +0300

    Alert_Updates

--- db/__init__.py
+++ db/__init__.py
@@ -29,6 +29,7 @@ from db.samples     import (
 from db.events      import (
     db_log_flap,
     db_load_flaps,
+    db_auto_resolve_flap,
     db_ack_flap,
     db_resolve_flap,
     db_resolve_all_flaps,
@@ -115,7 +116,7 @@ __all__ = [
     "db_load_history", "db_load_summary", "db_load_availability", "db_clean_samples",
     "db_rollup_backfill",
     # events
-    "db_log_flap", "db_load_flaps", "db_ack_flap", "db_resolve_flap", "db_resolve_all_flaps",
+    "db_log_flap", "db_load_flaps", "db_auto_resolve_flap", "db_ack_flap", "db_resolve_flap", "db_resolve_all_flaps",
     "db_log_trap", "db_load_traps", "db_clear_device_traps",
     "db_log_err", "db_load_err_logs", "db_clear_err_logs",
     "db_clear_sensor_err_logs",
